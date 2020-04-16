2e139aeb97744fa02c8ba436371870dd4a0e99b9
luca <luca.calabro@unimib.it>
2020-04-16 11:38:53 +0200

    Versione Funzionante

--- formuploadtogdrive/functions.py
+++ formuploadtogdrive/functions.py
@@ -3,9 +3,9 @@
 # restituisce una tupla con valori letti da ESSE3
 def dati_utente():
     # Questi dati andranno valorizzati leggendo da Esse3
-    MATRICOLA = "066666"
-    NOME = "PINCO"
-    COGNOME = "PALLINO"
+    MATRICOLA = "123456"
+    NOME = "TIZIO"
+    COGNOME = "CAIO"
     VERSIONE = "v1"
 
     return {"MATRICOLA": MATRICOLA, "NOME": NOME, "COGNOME": COGNOME, "VERSIONE": VERSIONE}
